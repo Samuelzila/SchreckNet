dad35100213bd959bfdeded0038110da8ed4d1c5
Samuelzila <54480547+Samuelzila@users.noreply.github.com>
2025-11-16 12:37:10 -0500

    Thematically appropriate landing page

--- public/index.php
+++ public/index.php
@@ -4,10 +4,33 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="assets/css/style.css">
+	<style>
+		html,
+		body {
+			margin: 0;
+			padding: 0;
+		}
+
+		#index-container {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			min-height: 100vh;
+			box-sizing: border-box;
+		}
+	</style>
 </head>
 
 <body>
-	<a href="login.php">click here</a>
+	<main>
+		<div id="index-container">
+			<a href="login.php">
+				<img src="assets/fingerprint.svg" alt="Fingerprint Icon" width="100" height="100" class="primary-color-filter">
+			</a>
+			<div class="gothic-text">Blood shall reveal the way...</div>
+		</div>
+	</main>
 </body>
 
 </html>
